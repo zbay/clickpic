a9e95a2f89fbdcd72cedf752c1407277264c4d09
zbay <zlwilliams1@gmail.com>
2016-02-19 18:18:25 +0000

    Added line for testing

--- client/js/ajax/newPic.js
+++ client/js/ajax/newPic.js
@@ -21,20 +21,21 @@ $("#newPicSubmit").on("click", function(){
         else{
              $("#newPicNonexistent").attr("style", "display:block");
         }
-        function supportedFormat(){
-            var theURL = $("[name=url]").val();
-            if((theURL.slice(-4) == ".jpg") || (theURL.slice(-5) == ".jpeg") || (theURL.slice(-4) == ".jpe") || (theURL.slice(-4) == ".jif")
-            || (theURL.slice(-4) == ".jfi") || (theURL.slice(-5) == ".jfif") || (theURL.slice(-4) == ".png") || (theURL.slice(-4) == ".gif")
-            || (theURL.slice(-4) == ".bmp") || (theURL.slice(-4) == ".dib") || (theURL.slice(-4) == ".svg") || (theURL.slice(-5) == ".svgz")
-            || (theURL.slice(-5) == ".apng") || (theURL.slice(-4) == ".ico") || (theURL.slice(-4) == ".jpe")){
-                return true
-            }
-            else{
-                return false;
-            }
-        }
     }
     else{
         $("#newPicError").attr("style", "display:block");
     }
 });
+function supportedFormat(){
+    var theURL = $("[name=url]").val();
+    console.log(theURL.slice(-4));
+        if((theURL.slice(-4) == ".jpg") || (theURL.slice(-5) == ".jpeg") || (theURL.slice(-4) == ".jpe") || (theURL.slice(-4) == ".jif")
+            || (theURL.slice(-4) == ".jfi") || (theURL.slice(-5) == ".jfif") || (theURL.slice(-4) == ".png") || (theURL.slice(-4) == ".gif")
+            || (theURL.slice(-4) == ".bmp") || (theURL.slice(-4) == ".dib") || (theURL.slice(-4) == ".svg") || (theURL.slice(-5) == ".svgz")
+            || (theURL.slice(-5) == ".apng") || (theURL.slice(-4) == ".ico") || (theURL.slice(-4) == ".jpe")){
+                return true
+        }
+        else{
+            return false;
+        }
+}
